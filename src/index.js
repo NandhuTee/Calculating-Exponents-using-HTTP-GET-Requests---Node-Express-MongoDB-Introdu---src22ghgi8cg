64fe605e241ddd5b366a79ab3bcd0da3db8bb7db
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-05-09 21:49:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+
+// Load environment variables from .env file if present
 dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
-  })
+  console.log(`Server is running on http://localhost:${port}`);
+});
